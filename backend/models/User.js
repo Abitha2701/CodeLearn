d7c7d471bfbcc843caff0d89a7a62a15197533f4
Abitha2701 <abitha27012005@gmail.com>
2025-08-07 21:33:58 +0530

    removed

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const express = require('express');
+
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },                          // ✅ Add required
